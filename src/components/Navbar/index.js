03787944305613875fdeca70773ab60f905c2ebf
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-10-20 18:26:13 +0300

    nav links added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import { Logo } from "assets/icons";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+
+    {
+      id: 2,
+      name: "About",
+      path: "/",
+    },
+    {
+      id: 4,
+      name: "Contact",
+      path: "/",
+    },
+  ];
+
   return (
     <header className="header">
       <div className="header__container">
@@ -8,6 +28,13 @@ const Navbar = () => {
           <div className="nav__logo">
             <Logo />
           </div>
+          <ul className="nav__list">
+            {links.map((link) => (
+              <li key={link.id} className="nav__link">
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
         </nav>
       </div>
     </header>
