d5ab808f969b7a30105f7910fc49fe02911b2468
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-03 03:09:39 +0300

    login page created

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,46 @@
 import React from "react";
-
+import Logingif from "assets/images/login-gif.mp4";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="login">
+      <div className="login__container">
+        <div className="login__video">
+          <video autoPlay loop muted src={Logingif}></video>
+        </div>
+        <div className="form">
+          <div className="form__title">
+            <h3>Sign in to Unemployed Youth</h3>
+          </div>
+          <form>
+            <div className="field">
+              <input
+                type="email"
+                className="field__input"
+                name="email"
+                placeholder="Email"
+              />
+              {/* <label className="field__label">Email</label> */}
+            </div>
+            <div className="field">
+              <input
+                type="password"
+                className="field__input"
+                name="password"
+                placeholder="Password"
+              />
+              {/* <label className="field__label">Password</label> */}
+            </div>
+          </form>
+          <div className="form__register">
+            <p>
+              Don't have any account?
+              <span>Sign Up</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
