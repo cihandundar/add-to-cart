309cd68c145258f9b1cbf65b691ce2631042dc74
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-14 00:55:48 +0300

    product component fix

---
+++ src/components/ProductItem/index.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Spinner from "assets/images/spinner.gif";
+import { Star } from "assets/icons";
+import { Link } from "react-router-dom";
+
+const Product = () => {
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products`)
+      .then((response) => setItem(response.data));
+  }, []);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 1300);
+
+  return (
+    <div className="container">
+      {loading && (
+        <div className="container__loading">
+          <img src={Spinner} alt="" />
+        </div>
+      )}
+      {item?.map((shopping) => (
+        <div key={shopping?.id} className="content">
+          <Link to={`/products/${shopping?.id}`}>
+            <div className="wrapper">
+              <div className="wrapper__image">
+                <img src={shopping?.image} alt="" />
+              </div>
+              <div className="wrapper__title">
+                <h4>{shopping?.title}</h4>
+              </div>
+              <div className="wrapper__text">
+                <p>{shopping?.description}</p>
+              </div>
+              <span>{shopping?.price} $</span>
+              <div className="star">
+                <Star />
+              </div>
+            </div>
+          </Link>
+          <div className="btn">
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Product;
