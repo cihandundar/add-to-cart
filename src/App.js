9c0a99e6f43a48f6276b73d2f4f033a00a51b953
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-14 00:53:55 +0300

    router fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "components";
-import { Basket, Home, Login } from "pages";
+import { Basket, Home, Login, Product, ProductDetails } from "pages";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/basket" element={<Basket />} />
       </Routes>
