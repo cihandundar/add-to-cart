a5537ef60cb4e56423f9bf3acc0c8e2cfebea5ab
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-14 22:53:32 +0300

    error page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Footer, Navbar } from "components";
-import { Basket, Home, Login, Product, ProductDetails } from "pages";
+import { Basket, Home, Login, Product, ProductDetails, Error } from "pages";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ function App() {
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/basket" element={<Basket />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/Error/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Errorimg from "assets/images/error.jpg";
+const Error = () => {
+  return (
+    <section
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      className="error"
+    >
+      <div style={{ height: "600px" }} className="error__container">
+        <img
+          style={{ width: "100%", height: "100%", objectFit: "contain" }}
+          src={Errorimg}
+          alt=""
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Error;
