fee07e8bcdd54aa14b5529dc01ac11b3be7785b8
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-14 22:55:58 +0300

    home page edited new components added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
+import { Hero, Info } from "components";
 import React from "react";
 
 const Home = () => {
   return (
     <main className="main">
-      <section className="section">home</section>
+      <section className="section">
+        <div className="section__container">
+          <div className="section__hero">
+            <Hero />
+          </div>
+          <div className="section__info">
+            <Info />
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
