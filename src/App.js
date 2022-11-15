0af1cd020e28619df78fde8c5256b1d0aa174879
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-16 02:31:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Footer, Navbar } from "components";
 import { Cart, Home, Login, Product, ProductDetails, Error } from "pages";
+
 function App() {
   return (
     <BrowserRouter>
@@ -11,7 +12,7 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/Cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
