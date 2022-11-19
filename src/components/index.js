4ffce6efd69bf6fda5276fbe0952af9cfb9d1e35
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-20 01:06:54 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import ProductItem from "./ProductItem";
 import Hero from "./Hero";
 import Info from "./Info";
+
 import Footer from "./Footer";
 
 export { Navbar, ProductItem, Footer, Hero, Info };
