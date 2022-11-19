8d915e9dad49958773ce3cec856b48c20b422143
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-20 02:24:17 +0300

    cart page add

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,49 @@
+import Descrease from "assets/icons/Descrease";
+import Increase from "assets/icons/Increase";
+import CartContext from "context/Context";
 import React from "react";
+import { useContext } from "react";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const context = useContext(CartContext);
+
+  console.log(context.state.cart.map((item) => item));
+
+  return (
+    <section className="cart">
+      <div className="cart__container">
+        {context?.state?.cart?.map((item) => (
+          <div className="cart__content">
+            <div className="cart__content__wrapper">
+              <div className="cart__content__img">
+                <img src={item?.image} alt="" />
+              </div>
+              <div className="cart__content__title">
+                <h3>{item?.title}</h3>
+                <span>$ {item?.price}</span>
+                <div className="cart__price">
+                  <p>
+                    Subtotal ( {item?.count} items ) : $
+                    {item?.price * item?.count}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="cart__btn">
+              <button>
+                <Increase />
+              </button>
+              <span>{item?.count}</span>
+
+              <button>
+                <Descrease />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
