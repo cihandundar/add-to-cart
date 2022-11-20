6e23295646cd3cc948e861230728ddcd6a12c932
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-21 01:11:20 +0300

    fix

--- src/assets/icons/RemoveIcon.js
+++ src/assets/icons/RemoveIcon.js
@@ -2,21 +2,19 @@ import * as React from "react";
 
 const RemoveIcon = (props) => (
   <svg
-    width={30}
-    height={30}
+    width={props.width || 25}
+    height={props.height || 25}
+    viewBox="0 0 30 30"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g clipPath="url(#a)" fill="#000">
-      <path d="m16.342 15 4.05-4.05a.833.833 0 0 0-1.175-1.175l-4.05 4.008-4.075-4.075a.833.833 0 0 0-1.175 1.175L13.983 15 10 18.933a.834.834 0 1 0 1.175 1.175l3.975-3.975 3.95 3.95a.833.833 0 0 0 1.175-1.175L16.342 15Z" />
-      <path d="M15 28.333a13.333 13.333 0 1 1 0-26.666 13.333 13.333 0 0 1 0 26.666Zm0-25a11.667 11.667 0 1 0 0 23.334 11.667 11.667 0 0 0 0-23.334Z" />
-    </g>
-    <defs>
-      <clipPath id="a">
-        <path fill="#fff" d="M0 0h30v30H0z" />
-      </clipPath>
-    </defs>
+    <path
+      d="m8.881 8.04 12.234 12.143M8.927 20.228 21.07 7.995"
+      stroke="#000"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+    />
   </svg>
 );
 
