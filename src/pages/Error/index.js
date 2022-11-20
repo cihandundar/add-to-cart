06cf17a3d052f5b80e6a65fa35b3554d2834c185
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-20 20:06:16 +0300

    height fix

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -4,7 +4,7 @@ const Error = () => {
   return (
     <section
       style={{
-        height: "100vh",
+        minHeight: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
