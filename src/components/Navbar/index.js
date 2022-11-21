4f4724c697f893565f648386266e3b66533bdfad
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-22 01:30:03 +0300

    mobile menu added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,8 +2,14 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import ShoppingIcon from "assets/icons/ShoppingIcon";
 import CartContext from "context/Context";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const context = useContext(CartContext);
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
   console.log(context.state.cart.length);
 
@@ -28,20 +34,26 @@ const Navbar = () => {
   return (
     <header className="header">
       <div className="header__container">
-        <nav className="nav">
+        <nav ref={navRef} className="nav">
           <div className="nav__logo">
-            <Link to="/">
+            <Link to="/" onClick={showNavbar}>
               <h2>Unemployed Youth</h2>
             </Link>
           </div>
           <ul className="nav__list">
             {links?.map((link) => (
-              <li key={link?.id} className="nav__link">
+              <li key={link?.id} className="nav__link" onClick={showNavbar}>
                 <Link to={link?.path}>{link?.name}</Link>
               </li>
             ))}
           </ul>
+          <button className="nav__btn nav__closebtn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
         </nav>
+        <button className="nav__btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
         <span className="nav__icon">
           <Link to="/cart">
             <span>
