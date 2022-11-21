900e34265bbc6780756cc93821b3438bd6a2c5ad
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-22 01:30:27 +0300

    error page fix

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -3,43 +3,13 @@ import Errorimg from "assets/images/error.svg";
 import { Link } from "react-router-dom";
 const Error = () => {
   return (
-    <section
-      style={{
-        minHeight: "80vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-      className="error"
-    >
-      <div
-        style={{ height: "600px", position: "relative" }}
-        className="error__container"
-      >
-        <img
-          style={{ width: "100%", height: "100%", objectFit: "contain" }}
-          src={Errorimg}
-          alt=""
-        />
-        <Link
-          style={{
-            position: "absolute",
-            top: "20%",
-            left: "39%",
-            background: "#ff735c",
-            width: "250px",
-            height: "45px",
-            color: "#ffff",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: "30px",
-            fontWeight: "500",
-            letterSpacing: "3px",
-          }}
-          to="/"
-        >
-          Go to Homepage
+    <section className="error">
+      <div className="error__container">
+        <div className="error__image">
+          <img src={Errorimg} alt="" />
+        </div>
+        <Link to="/">
+          <button> Go to Homepage</button>
         </Link>
       </div>
     </section>
