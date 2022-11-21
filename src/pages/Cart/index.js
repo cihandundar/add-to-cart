cf90bd44225e5d903b776e144b78e0b55d03912f
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-11-21 17:36:14 +0300

    cart page fix

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import CartContext from "context/Context";
 import { useContext } from "react";
 import { DescreaseIcon, IncreaseIcon, RemoveIcon } from "assets/icons";
-
+import { Link } from "react-router-dom";
+import Empty from "assets/images/empty.jpg";
 const Cart = () => {
   const context = useContext(CartContext);
   const { decrease, increase, removeFromCart, totalCart } =
@@ -11,40 +12,66 @@ const Cart = () => {
 
   return (
     <section className="cart">
-      <h1>{totalCart}</h1>
-      <div className="cart__container">
-        {context?.state?.cart?.map((item) => (
-          <div className="cart__content">
-            <div className="cart__content__wrapper">
-              <div className="cart__content__img">
-                <img src={item?.image} alt="" />
+      {context?.state?.cart > [0] ? (
+        <div className="cart__main">
+          <div className="cart__container">
+            {context?.state?.cart?.map((item) => (
+              <div className="cart__content">
+                <div className="cart__content__wrapper">
+                  <div className="cart__content__img">
+                    <img src={item?.image} alt="" />
+                  </div>
+                  <div className="cart__content__title">
+                    <h4>{item?.title}</h4>
+                    <span>Price : $ {item?.price}</span>
+                    <div className="cart__price">
+                      <p>
+                        Subtotal ( {item?.count} items ) : $
+                        {(item?.price * item?.count).toFixed(2)}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="cart__btn">
+                  <button onClick={() => decrease(item.id)}>
+                    <DescreaseIcon />
+                  </button>
+                  <span>{item?.count}</span>
+                  <button onClick={() => increase(item.id)}>
+                    <IncreaseIcon />
+                  </button>
+                </div>
+                <button
+                  onClick={() => removeFromCart(item.id)}
+                  className="remove"
+                >
+                  <RemoveIcon />
+                </button>
               </div>
-              <div className="cart__content__title">
-                <h4>{item?.title}</h4>
-                <span>Price : $ {item?.price}</span>
-                <div className="cart__price">
-                  <p>
-                    Subtotal ( {item?.count} items ) : $
-                    {(item?.price * item?.count).toFixed(2)}
-                  </p>
+            ))}
+          </div>
+          <div className="cart__total">
+            <div className="cart__total__wrapper">
+              <div className="cart__total__border">
+                <h3> Subtotal: ${totalCart}</h3>
+                <div className="cart__total__btn">
+                  <Link to="/login">Continue to checkout</Link>
                 </div>
               </div>
             </div>
-            <div className="cart__btn">
-              <button onClick={() => decrease(item.id)}>
-                <DescreaseIcon />
-              </button>
-              <span>{item?.count}</span>
-              <button onClick={() => increase(item.id)}>
-                <IncreaseIcon />
-              </button>
-            </div>
-            <button onClick={() => removeFromCart(item.id)} className="remove">
-              <RemoveIcon />
-            </button>
           </div>
-        ))}
-      </div>
+        </div>
+      ) : (
+        <div className="empty">
+          <div className="empty__image">
+            <img src={Empty} alt="" />
+          </div>
+          <div className="empty__text">
+            <h4>Your cart is currently empty!</h4>
+            <Link to="/">Return to Home</Link>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
